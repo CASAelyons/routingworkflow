3461cae873ed993ca6a695d0118e004772f53515
Eric Lyons <elyons@engin.umass.edu>
2020-02-24 10:14:53 -0600

    removed username passwords from code. Defaulting to NFS approach for container. No need for input files, all command line/REST interfcace

--- gen_route_dax.py
+++ gen_route_dax.py
@@ -10,35 +10,37 @@ from Pegasus.DAX3 import *
 from datetime import datetime
 from argparse import ArgumentParser
 
-CASA_AUTH = ("admin","shabiz")
-
 class routingWorkflow(object):
-    #def __init__(self, outdir, queryURL, usrname, password):
-    def __init__(self, outdir, featurename):
+    def __init__(self, outdir, featurename, flights, obstacles, outputFlightURL, usrname, password):
         self.outdir = outdir
         self.featurename = featurename
-        #self.queryURL = queryURL
-        #self.usrname = usrname
-        #self.password = password
+        self.flights = flights
+        self.obstacles = obstacles
+        self.outputFlightURL = outputFlightURL
+        self.usrname = usrname
+        self.password = password
 
     def generate_dax(self):
         
         ts = datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
         dax = ADAG("casa_routing_wf-%s" % ts)
         dax.metadata("name", "CASA Routing")
-        print ("casa_routing_wf-%s" % ts)
-        #print ("endpoint %s" % ep)
-        
-        routing_configfile = File("route_plan.cfg")
+
+        # Call the flight routing algorithm
+        usrpass = self.usrname + ":" + self.password
         routing_job = Job("route_plan")
-        routing_job.addArguments("-c", routing_configfile);
-        routing_job.addArguments("-n", self.featurename);
-        routing_job.uses(routing_configfile, link=Link.INPUT)
+        routing_job.addArguments("-n", self.featurename)
+        routing_job.addArguments("-l", self.flights)
+        routing_job.addArguments("-o", self.obstacles) 
+        routing_job.addArguments("-t", self.outputFlightURL) 
+        routing_job.addArguments("-u", usrpass)
+        dax.addJob(routing_job)
 
         # Write the DAX file
         daxfile = os.path.join(self.outdir, dax.name+".dax")
         dax.writeXMLFile(daxfile)
-        print daxfile
+        pegasus_call = os.system("./plan.sh " + daxfile)
+        print pegasus_call
 
     def generate_workflow(self):
         # Generate dax
@@ -46,18 +48,26 @@ class routingWorkflow(object):
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="Flight Path Routing Workflow")
-    parser.add_argument("-f", "--flights", metavar="FLIGHT_QUERY_URL", type=str, help="URL to query flights", required=True)
-    #parser.add_argument("-u", "--usrname", metavar="USR", type=str, help="username for query authentication", required=True)
-    #parser.add_argument("-p", "--password", metavar="PASS", type=str, help="password for query authentication", required=True)
-    parser.add_argument("-o", "--outdir", metavar="OUTPUT_LOCATION", type=str, help="DAX Directory", required=True)
+    parser.add_argument("-l", "--flights", metavar="FLIGHT_QUERY_URL", type=str, help="URL to query flights", required=True)
+    parser.add_argument("-o", "--obstacles", metavar="OBSTACLE_QUERY_URL", type=str, help="URL to query obstacles", required=True)
+    parser.add_argument("-t", "--outputFlightURL", metavar="OUTPUT_FLIGHT_URL", type=str, help="URL to post output", required=True)
+    parser.add_argument("-u", "--usrname", metavar="USR", type=str, help="username for query authentication", required=True)
+    parser.add_argument("-p", "--password", metavar="PASS", type=str, help="password for query authentication", required=True)
+    parser.add_argument("-d", "--outdir", metavar="OUTPUT_LOCATION", type=str, help="DAX Directory", required=True)
     args = parser.parse_args()
     outdir = os.path.abspath(args.outdir)
 
     if not os.path.isdir(args.outdir):
         os.makedirs(outdir)
-    url = args.flights
+
+    flights = args.flights
+    obstacles = args.obstacles
+    outputFlightURL = args.outputFlightURL
+    usrname = args.usrname
+    password = args.password
     
-    response = requests.get(url, auth=CASA_AUTH, verify=True)
+    CASA_AUTH = (usrname,password) 
+    response = requests.get(flights, auth=CASA_AUTH, verify=True)
     if response.status_code == 200:
         liveEvents = geojson.loads(response.content)
     
@@ -66,6 +76,7 @@ if __name__ == '__main__':
         if feature['geometry']['type'] == 'LineString':
             featurename = feature['properties']['eventName']
             print featurename
-            workflow = routingWorkflow(outdir,featurename)
+            workflow = routingWorkflow(outdir,featurename,flights,obstacles,outputFlightURL,usrname,password)
             workflow.generate_workflow()
 
+            
