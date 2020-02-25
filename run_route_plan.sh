8a7393fcfcf3cd1369b0020e42076fbb531afaaf
Eric Lyons <elyons@cs.umass.edu>
2020-02-25 16:14:04 -0500

    updated for no data staging

--- run_route_plan.sh
+++ run_route_plan.sh
@@ -2,6 +2,6 @@
 
 dax_name=$(python gen_route_dax.py -d dax_outputs -l "https://emmy8.casa.umass.edu:8091/liveEvents" -o "https://hazard.hpcc.umass.edu:8091/alertArchiveOverlap?start=2020-02-04T05:55:00Z&end=2020-02-04T06:00:00Z&hazard=STORM_CASA_5&format=json" -t "https://emmy8.casa.umass.edu:8091/casaAlert/flightPath" -u admin -p shabiz)
 
-#echo $dax_name;
-./plan.sh ${dax_name}
+echo $dax_name;
+#./plan.sh ${dax_name}
 
