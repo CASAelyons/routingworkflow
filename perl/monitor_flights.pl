67c59f811c29e56fd5692a2d25577c9e873e07e9
Eric Lyons <elyons@engin.umass.edu>
2020-09-18 16:48:02 -0500

    added new branch for uam routing
    Works with non-included cgi script that creates dax jobs.  Branch mainly a stub.  Updates to follow

---
+++ perl/monitor_flights.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/perl -w
+
+##################################################################################################
+#####WRITTEN BY ERIC LYONS 8/2020 for CASA, UNIVERSITY OF MASSACHUSETTS##########################
+##################################################################################################
+#  TESTED FUNCTIONALITY:         
+#  Monitors incoming flight requests
+# 
+#  #                                                                                                  #
+##################################################################################################
+
+use POSIX qw(setsid);
+use File::Monitor;
+
+use threads;
+use threads::shared;
+
+our $input_data_dir;
+##Parse Command Line
+&command_line_parse;
+
+&daemonize;
+
+##Realtime Mode -- Gets MCC stream
+my $file_mon = new threads \&file_monitor;
+
+sleep 900000000;
+
+sub file_monitor {
+    
+    my $dir_monitor = File::Monitor->new();
+        
+    $dir_monitor->watch( {
+	name        => "$input_data_dir",
+	recurse     => 1,
+        callback    => \&new_files,
+    } );
+    
+    $dir_monitor->scan;
+    
+    for ($i=0; $i < 9000000000; $i++) {
+	my @changes = $dir_monitor->scan;   
+	sleep 1;
+    }
+    
+    sub new_files 
+    {
+	my ($name, $event, $change) = @_;
+	@new_dax_files = $change->files_created;
+	my @dels = $change->files_deleted;
+	print "Added: ".join("\nAdded: ", @new_dax_files)."\n" if @new_dax_files;
+	foreach $file (@new_dax_files) {
+	    my $pathstr;
+            my $filename;
+            ($pathstr, $filename) = $file =~ m|^(.*[/\\])([^/\\]+?)$|;
+            my $suffix = substr($filename, -3, 3);
+
+	    if ($suffix eq "dax") {
+		my $plancall = "/home/ldm/routingworkflow/plan_route.sh $file";
+		system($plancall);
+		sleep 1;
+		
+	    }
+	}
+    }
+}
+
+sub daemonize {
+    chdir '/'                 or die "Can't chdir to /: $!";
+    open STDIN, '/dev/null'   or die "Can't read /dev/null: $!";
+    open STDOUT, '>>/dev/null' or die "Can't write to /dev/null: $!";
+    open STDERR, '>>/dev/null' or die "Can't write to /dev/null: $!";
+    defined(my $pid = fork)   or die "Can't fork: $!";
+    exit if $pid;
+    setsid                    or die "Can't start a new session: $!";
+    umask 0;
+}
+
+sub command_line_parse {
+    if ($#ARGV < 0) { 
+	print "Usage:  plan_route.pl <daxfile>\n";
+   	exit; 
+    }
+    $input_data_dir = $ARGV[0];
+    
+    my @rdd = split(/ /, $input_data_dir);
+    foreach $w (@rdd) {
+	print "Will recursively monitor $w for incoming dax files\n";
+    }
+    
+}
